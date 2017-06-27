d6911568babc25d5ec46f0f8f027d093c27528ab
Patrick Foh <patrickfoh@gmail.com>
2017-06-27 11:03:11 +0100

    Updated selector name

--- src/app/main/sales/sidenav/sidenav.component.ts
+++ src/app/main/sales/sidenav/sidenav.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-sidenav',
+  selector: 'app-sales-sidenav',
   templateUrl: './sidenav.component.html',
   styleUrls: ['./sidenav.component.scss']
 })
