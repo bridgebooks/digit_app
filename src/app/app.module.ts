71e1ec56f60d42e2dc3c641d7c9ceb7b39630a08
Patrick <patrickfoh@gmail.com>
2017-08-02 07:08:31 +0100

    Refactor module definition

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,40 @@
-import { BrowserModule, Title } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule, Title } from '@angular/platform-browser';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
 
-import { AppRoutingModule } from './app-routing.module';
+import { LocalStorageModule } from 'angular-2-local-storage';
+import { JwtHelper } from 'angular2-jwt';
+
 import { AppComponent } from './app.component';
 import { AuthGuard } from './shared';
+import { JwtInterceptor } from './shared';
+
+import { JwtService } from './services';
+
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
+    AppComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     FormsModule,
-    HttpModule,
+    LocalStorageModule.withConfig({
+      prefix: 'zb',
+      storageType: 'localStorage'
+    }),
     AppRoutingModule
   ],
-  providers: [Title, AuthGuard],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+    Title,
+    JwtHelper, 
+    JwtService, 
+    AuthGuard
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
