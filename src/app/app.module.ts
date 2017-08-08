d999f67b96f75da8444941a7003392ff138ef650
Patrick <patrickfoh@gmail.com>
2017-08-08 09:16:08 +0100

    Updated app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,16 @@ import { BrowserModule, Title } from '@angular/platform-browser';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SimpleNotificationsModule } from 'angular2-notifications';
 
 import { LocalStorageModule } from 'angular-2-local-storage';
 import { JwtHelper } from 'angular2-jwt';
 
 import { AppComponent } from './app.component';
 import { AuthGuard } from './shared';
-import { JwtInterceptor } from './shared';
+import { HttpErrorInterceptor, JwtInterceptor } from './shared';
 
-import { JwtService } from './services';
+import { AlertService, JwtService } from './services';
 
 import { AppRoutingModule } from './app-routing.module';
 
@@ -28,10 +29,13 @@ import { AppRoutingModule } from './app-routing.module';
       prefix: 'zb',
       storageType: 'localStorage'
     }),
+    SimpleNotificationsModule.forRoot(),
     AppRoutingModule
   ],
   providers: [
+    AlertService,
     { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: HttpErrorInterceptor, multi: true },
     Title,
     JwtHelper, 
     JwtService, 
