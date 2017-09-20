df645eeea9b4f0f87a85dc38e2cec90d00f117b8
Patrick <patrickfoh@gmail.com>
2017-09-20 06:05:14 +0100

    Update component definition

--- src/app/main/settings/chart-accounts/chart-accounts.component.ts
+++ src/app/main/settings/chart-accounts/chart-accounts.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { SessionService, OrgService } from '../../../services';
+import { AlertService, SessionService, OrgService, AccountsService } from '../../../services';
 import { State } from 'clarity-angular/data/datagrid'
 import { Account } from '../../../models/data/account';
 
@@ -25,24 +25,67 @@ export class ChartAccountsComponent implements OnInit {
 
   enableBulkOptions: boolean = false;
   deleteConfirmModalVisible: boolean = false;
-  contactDeleteProcessing: boolean = false;
+  accountDeleteProcessing: boolean = false;
   deleteBtnDisabled: boolean = false;
   toDelete: Account[] | Account | null;
 
-  constructor(private session: SessionService, private orgService: OrgService) { }
-
-  private 
+  constructor(private alertService: AlertService, private session: SessionService, private orgService: OrgService, private accountService: AccountsService) { }
   
   onDeleteSelected($event) {
     this.delete(this.selected);
   }
 
-  delete(selection: Account | Account[]) {
+  delete(selection: Account[] | Account) {
     this.deleteConfirmModalVisible = true;
+
+    if (Array.isArray(selection)) {
+      this.toDelete = selection.filter((account) => {
+        return !account.is_system;
+      });
+    }
+
     this.toDelete = selection;
   }
 
   deleteSelection() {
+    this.accountDeleteProcessing = true;
+    this.deleteBtnDisabled = true;
+
+    if (Array.isArray(this.toDelete)) {
+      const ids = []
+      this.toDelete.forEach(contact => ids.push(contact.id)) 
+      this.accountService
+        .deleteMany(ids)
+        .subscribe(response => {
+          this.alertService.success('Delete', 'Account(s) successfully deleted', { timeOut: 5000 })
+          this.deleteConfirmModalVisible = false;
+          this.deleteBtnDisabled = false;
+          this.accountDeleteProcessing = false;
+          this.toDelete = []
+
+          this.refresh({})
+        }, err => {
+          this.deleteConfirmModalVisible = false;
+          this.deleteBtnDisabled = false;
+          this.accountDeleteProcessing = false;
+        })
+    } else {
+      this.accountService
+        .delete(this.toDelete.id)
+        .subscribe(response => {
+          this.alertService.success('Delete', 'Contact successfully deleted', { timeOut: 5000 })
+          this.deleteConfirmModalVisible = false;
+          this.deleteBtnDisabled = false;
+          this.accountDeleteProcessing = false;
+          this.toDelete = []
+
+          this.refresh({})
+        }, err => {
+          this.deleteConfirmModalVisible = false;
+          this.deleteBtnDisabled = false;
+          this.accountDeleteProcessing = false;
+        })
+    }
   }
 
   onSelectedChange(selected: Account[]) {
