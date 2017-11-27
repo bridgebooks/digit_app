9ac739cb5177b9eb2a555b549ee5d1465da3cd2d
ADMIN <admin@ADMINs-MacBook-Pro.local>
2017-11-28 00:01:17 +0100

    Add payrun route

--- src/app/main/payroll/payroll-routing.module.ts
+++ src/app/main/payroll/payroll-routing.module.ts
@@ -4,6 +4,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { EmployeeListComponent } from './employee-list/employee-list.component';
 import { EmployeeFormComponent } from './employee-form/employee-form.component';
 import { EmployeeDetailComponent } from './employee-detail/employee-detail.component';
+import { PayrunsComponent } from 'app/main/payroll/payruns/payruns.component';
 
 const routes: Routes = [
     { 
@@ -32,6 +33,13 @@ const routes: Routes = [
         component: EmployeeDetailComponent,
         data: {
             title: 'Employee - Bridge Books'
+        }
+    },
+    {
+        path: 'runs',
+        component: PayrunsComponent,
+        data: {
+            title: 'Payruns - Bridge Books'
         }
     }
 ];
