a8166a901d3a4d873beaee40e51d46e2ac15e17c
ADMIN <admin@ADMINs-MacBook-Pro.local>
2017-11-28 00:01:35 +0100

    Add payrun pieces

---
+++ src/app/main/payroll/payruns/payruns.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-payruns',
+  templateUrl: './payruns.component.html',
+  styleUrls: ['./payruns.component.scss']
+})
+export class PayrunsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
