60c74cabb4d8ac115ddc989718f801e03c919d4c
ADMIN <admin@ADMINs-MacBook-Pro.local>
2017-11-30 12:29:30 +0100

    Define service methods

--- src/app/services/payrun.service.ts
+++ src/app/services/payrun.service.ts
@@ -1,8 +1,23 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { environment } from '../../environments/environment';
 
 @Injectable()
 export class PayrunService {
 
-  constructor() { }
+  baseUrl: string = environment.apiUrl + 'payruns';
 
+  constructor(private httpClient: HttpClient) { }
+
+  create(body: object) {
+  }
+
+  update(id: string, body: object) {
+  }
+
+  get(id: string) {
+  }
+
+  delete(id: string) {
+  }
 }
