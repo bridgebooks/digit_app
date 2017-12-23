f23da0e448a72bf0031691046395ca2673a6dde2
Patrick Foh <patrickfoh@gmail.com>
2017-12-23 14:25:10 +0100

    Add security check

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit, AfterContentInit, OnDestroy } from '@angular/core';
+import { ViewChild, Component, OnInit, AfterContentInit, OnDestroy } from '@angular/core';
 import { Router, NavigationEnd, NavigationStart, ActivatedRoute } from '@angular/router';
+import { Idle, DEFAULT_INTERRUPTSOURCES } from '@ng-idle/core';
+import { environment } from '../../environments/environment';
 import { MainNavigation } from './main.navigation';
 import { EventbusService, JwtService, AuthService, SessionService } from '../services';
 import { Subscription } from 'rxjs'
@@ -9,6 +11,8 @@ import 'clarity-icons/shapes/core-shapes';
 import 'clarity-icons/shapes/commerce-shapes';
 import 'clarity-icons/shapes/essential-shapes';
 import 'clarity-icons/shapes/technology-shapes';
+import { Modal } from 'clarity-angular';
+import { Subject } from 'rxjs/Subject';
 
 @Component({
     selector: 'app-main',
@@ -21,39 +25,56 @@ export class MainComponent implements OnInit, AfterContentInit, OnDestroy {
     navCollasped: boolean = false;
 
     user: any;
-
     org: any;
+    security: any = {}
+    securityCheck$: Subject<any> = new Subject();
     
-    logoutModalVisible: boolean = false;
+    @ViewChild('logoutModal') logoutModal: Modal;
+    @ViewChild('loginModal') loginModal: Modal;
 
     logoutProcessing: boolean = false;
-
-    logoutBtnDisabled: boolean = false;
-
+    checking: boolean = false;
+    loginModalOpen: boolean = false;
     aclAlertShow: boolean = false;
 
     aclErrorSub: Subscription;
 
     constructor(
+        private idleMonitor: Idle,
         public router: Router, 
         public activatedRoute: ActivatedRoute,
         private eventbus: EventbusService,
         private jwtService: JwtService, 
         private session: SessionService, 
         private authService: AuthService) {
+        // configure idle state monitor
+        this.idleMonitor.setIdle(environment.idleTime);
+        this.idleMonitor.setTimeout(environment.idleTimeout);
+        this.idleMonitor.setInterrupts(DEFAULT_INTERRUPTSOURCES);
+        
+        this.idleMonitor.onTimeout.subscribe(() => {
+            if (!this.loginModal._open) this.loginModalOpen = true;
+        })
     }
 
-    showLogoutModal() {
-        this.logoutModalVisible = true;
-    }
+    securityCheck() {
+        this.checking = true;
 
-    hideLogoutModal() {
-        this.logoutModalVisible = false;
+        this.authService
+            .login(this.security)
+            .subscribe(response => {
+                this.jwtService.saveToken(response.data.token);
+                this.idleMonitor.watch();
+                this.securityCheck$.next(true);
+            }, err => {
+                this.checking = false;
+            }, () => {
+                this.checking = false;
+            })
     }
 
     logout() {
         this.logoutProcessing = true;
-        this.logoutBtnDisabled = true;
 
         this.authService
             .logout()
@@ -61,22 +82,32 @@ export class MainComponent implements OnInit, AfterContentInit, OnDestroy {
                 if (response.status === 'success') {
                     this.logoutProcessing = false;
                     this.session.end();
-                    this.hideLogoutModal();
+                    this.logoutModal.close();
+                    this.idleMonitor.stop();
                     this.router.navigate(['/login'])
                 }
             },
             err => {
                 this.logoutProcessing = false;
-                this.logoutBtnDisabled = false;
-                this.hideLogoutModal();
+                this.logoutModal.close();
             })
     }
 
     ngOnInit() {
         this.org = this.session.getDefaultOrg()
+        this.user = this.session.getUser();
+        this.security.email = this.user.email;
+
+        this.idleMonitor.watch();
+
         this.aclErrorSub = this.eventbus.subscribe('acl:error', (payload) => {
             this.aclAlertShow = true;
             setTimeout(() => { this.aclAlertShow = false; }, 5000);
+        })
+
+        this.securityCheck$.subscribe(() => {
+            this.loginModalOpen = false;
+            this.security.password = null;
         })
         
         if (this.router.url === '/') {
@@ -89,5 +120,6 @@ export class MainComponent implements OnInit, AfterContentInit, OnDestroy {
 
     ngOnDestroy() {
         this.aclErrorSub.unsubscribe();
+        this.securityCheck$.unsubscribe();
     }
 }
