cef7eee1d3ee926d159349917db2805d3b20602f
Patrick Foh <patrickfoh@gmail.com>
2018-01-12 00:10:37 +0100

    Add reports routes

--- src/app/main/reports/reports-routing.module.ts
+++ src/app/main/reports/reports-routing.module.ts
@@ -1,6 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ReportsComponent } from './reports.component';
+import { AgedReceivablesComponent } from './aged-receivables/aged-receivables.component';
+import { AgedPayablesComponent } from './aged-payables/aged-payables.component';
+import { BalanceSheetComponent } from './balance-sheet/balance-sheet.component';
+import { ProfitLossComponent } from './profit-loss/profit-loss.component';
 
 const routes: Routes = [
   { 
@@ -11,6 +15,38 @@ const routes: Routes = [
       acl: 'reports'
     }
   },
+  {
+    path: 'balance-sheet',
+    component: BalanceSheetComponent,
+    data: {
+      title: 'Balance Sheet - Bridgebooks',
+      acl: 'reports'
+    }
+  },
+  {
+    path: 'profit-loss',
+    component: ProfitLossComponent,
+    data: {
+      title: 'Profit & Loss - Bridgebooks',
+      acl: 'reports'
+    }
+  },
+  {
+    path: 'aged-receivables',
+    component: AgedReceivablesComponent,
+    data: {
+      title: 'Aged Receivables - Bridgebooks',
+      acl: 'reports'
+    }
+  },
+  {
+    path: 'aged-payables',
+    component: AgedPayablesComponent,
+    data: {
+      title: 'Aged Payables - Bridgebooks',
+      acl: 'reports'
+    }
+  }
 ]
 
 @NgModule({
