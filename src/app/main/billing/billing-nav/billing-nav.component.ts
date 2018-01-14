ec4407e85927db0630927fb3dbf956b93c7b923c
Patrick Foh <patrickfoh@gmail.com>
2018-01-14 10:00:19 +0100

    Add BillingNav Component

---
+++ src/app/main/billing/billing-nav/billing-nav.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-billing-nav',
+  templateUrl: './billing-nav.component.html',
+  styleUrls: ['./billing-nav.component.scss']
+})
+export class BillingNavComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
