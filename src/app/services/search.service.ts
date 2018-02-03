a15c21d8e37ff828aa80713c0efb84f54bb3e4ae
Patrick Foh <patrickfoh@gmail.com>
2018-02-03 10:20:11 +0100

    Update import

--- src/app/services/search.service.ts
+++ src/app/services/search.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { environment } from '../../environments/environment';
-import { Observable } from 'rxjs/observable';
+import { Observable } from 'rxjs/Observable';
 import { SearchResponse } from '../models/responses/search';
 
 import 'rxjs/add/operator/debounceTime';
