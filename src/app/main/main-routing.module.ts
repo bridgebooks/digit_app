d9e3f10cf85a020e8c5f3a3344de4b078a9f17a6
Patrick Foh <patrickfoh@gmail.com>
2018-02-10 09:33:16 +0100

    Register ledger module routes

--- src/app/main/main-routing.module.ts
+++ src/app/main/main-routing.module.ts
@@ -73,8 +73,16 @@ const routes: Routes = [
                 canActivate: [AuthGuard],
                 data: {
                     title: 'General Settings - Bridgebooks',
-                    showSideNav: true,
-                    sidenavMenu: 'settings'
+                    showSideNav: true
+                }
+            },
+            {
+                path: 'ledger',
+                loadChildren: './ledger/ledger.module#LedgerModule',
+                canActivate: [AuthGuard],
+                data: {
+                    title: 'Ledger - Bridgebooks',
+                    showSideNav: true
                 }
             }
         ]
