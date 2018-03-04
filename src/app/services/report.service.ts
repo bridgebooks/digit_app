c8ce872b798b2cfab03665b09e178960b5b5b52c
Patrick Foh <patrickfoh@gmail.com>
2018-03-04 06:04:59 +0100

    Profit loss report

--- src/app/services/report.service.ts
+++ src/app/services/report.service.ts
@@ -3,6 +3,7 @@ import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { environment } from '../../environments/environment';
 import { SubscriptionPlan } from '../models/data/subscription-plan';
 import { BalanceSheetReport } from '../models/responses/balance-sheet';
+import { ProfitLossReport } from '../models/responses/profit-loss';
 
 interface BalanceSheetOptions {
   export_pdf?: boolean;
@@ -26,5 +27,19 @@ export class ReportService {
     }
 
     return this.http.get<BalanceSheetReport>(url, { headers, params })
+  }
+
+  profitLoss(id: string, options: object) {
+    const headers = new HttpHeaders().set('Content-Type', 'application/json');
+    const url = `${this.baseUrl}/${id}/profit-loss`;
+    let params = new HttpParams();
+
+    if (options) {
+      Object.keys(options).forEach(key => {
+        params = params.append(key, options[key]);
+      })
+    }
+
+    return this.http.get<ProfitLossReport>(url, { headers, params })
   }
 }
